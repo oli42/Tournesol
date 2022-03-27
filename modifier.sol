62491543cdf5ec7298683af3ba9756b85ca7e82e
olivier <olivier.chicheportiche@gmail.com>
2022-03-27 12:33:50 +0200

    modifier

---
+++ modifier.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier:UNLICENSED
+
+pragma solidity 0.8.7;
+
+contract modif{
+    
+    address _owner;
+    uint _nbr;
+
+    constructor(){
+        _owner = msg.sender;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == _owner, "Not the _owner");
+        _;  // to continue the calling function
+    }
+
+    function getNbr() public view isOwner returns(uint){
+        return _nbr;
+    }
+
+    function setNbr(uint nbr) public isOwner{
+       _nbr = nbr;
+    }
+}
